7b82b5e7bcb28f4dc0e655827cfbff532af741cf
adrianrezha29 <adrianrezha29@gmail.com>
2022-11-18 17:01:54 +0700

    add new node.next

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -28,9 +28,30 @@ namespace double_linked_list
         public void addNode()
         {
             int nim;
-            string nim;
+            string nm;
             Console.WriteLine("\nEnter the roll number of the student: ");
             nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.next = null;
+                START = newNode;
+                return;
+            }
         }
     }
 }
