e9213fee75b2c624e069b4fc04b7286a78612d87
adrianrezha29 <adrianrezha29@gmail.com>
2022-11-18 17:39:46 +0700

    add public bool list empty

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -117,7 +117,24 @@ namespace double_linked_list
             current.next.prev = previous;
             return true;
         }
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
         public bool ascending()
-
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecord in the ascending order of" + "Roll number are:\n");
+                Node currentNode;
+                for(currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.WriteLine()
+            }
+        }
     }
 }
