891a94dd51b791691dce1e874fd1d2507010fa1a
adrianrezha29 <adrianrezha29@gmail.com>
2022-11-18 17:15:41 +0700

    add node previous ketinggalan

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -52,6 +52,18 @@ namespace double_linked_list
                 START = newNode;
                 return;
             }
+            /*if the node is to be inserted at between two node*/
+            Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
             /*On the execution of the above for loop, prev and
              * current will point to those nodes
              * between which the new  node is to be inserted*/
